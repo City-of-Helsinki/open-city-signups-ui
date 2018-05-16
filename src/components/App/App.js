cff61f06577ce209abb37c8299c3313a2bb40774
Olaoluwa Alabi <ayoalabi0@gmail.com>
2018-05-16 14:10:03 +0300

    react-router modified

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,50 +1,28 @@
 import React, {Component} from 'react'
-import { BrowserRouter, Redirect, Route, Switch} from 'react-router-dom';
+import {BrowserRouter, Redirect, Route, Switch} from 'react-router-dom';
 
-import logo from '../../assets/logo.svg'
 import './App.css'
+
+import LandingPage from '../Landingpage/Landing';
+import NotFound from '../Notfound/Notfound';
 
 class App extends Component {
 
-	Home = props => {
-		return (
-		  <div className='App'>
-				<header className='App-header'>
-					<img src={logo} className='App-logo' alt='logo' />
-					<h1 className='App-title'>Welcome to React</h1>
-				</header>
-				<p className='App-intro'>
-				{props.title}
-				</p>
-      		</div>
-		)
-	  }
-	  
-	  NotFound = () => {
-		return (
-		  <div className='App'>
-			  <p className='intro'>
-				Page not Found!!!
-			  </p>
-			</div>
-		)
-	  }
   render() {
     return (
-	  <BrowserRouter>
-				<Switch>
-					<Route path='/signup' render={()=><this.Home title='Home Page'/>}/>
-					<Route path='/about' render={()=><this.Home title='About Page'/>}/>
-					<Route path='/help' render={()=><this.Home title='Help Page'/>}/>
-					<Route path='/services' render={()=><this.Home title='Services Page'/>}/>ok
-					<Redirect from='/' to = '/signup' exact={true} />
-					<Route component={this.NotFound}/>
-				</Switch>
-			</BrowserRouter>
+      <BrowserRouter>
+        <Switch>
+          <Route path='/signup' render={()=><LandingPage title='Home Page'/>}/>
+          <Route path='/about' render={()=><LandingPage title='About Page'/>}/>
+          <Route path='/help' render={()=><LandingPage title='Help Page'/>}/>
+          <Route path='/services' render={()=><LandingPage title='Services Page'/>}/>
+          <Redirect from='/' to = '/signup' exact={true} />
+          <Route path='/404' render={()=><NotFound/>}/>
+          <Redirect from='*' to = '/404' exact={true} />
+        </Switch>
+      </BrowserRouter>
     );
   }
 }
-
-
 
 export default App;
---
+++ src/components/Notfound/Notfound.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './notfound.css';
+
+const NotFound = props =>{
+  return(
+    <div className='App'>
+      <p className='intro'>
+        Page not Found!!!
+      </p>
+    </div>
+  );
+};
+
+export default NotFound;
