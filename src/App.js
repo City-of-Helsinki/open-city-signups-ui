bbb22e6eed3d89cc0f7ee3df5379c17a0d0a0fbe
Olaoluwa Alabi <ayoalabi0@gmail.com>
2018-05-16 11:36:00 +0300

    React-router v4 added

--- src/App.js
+++ src/App.js
@@ -1,19 +1,45 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter, Redirect, Route, Switch} from 'react-router-dom';
 
 class App extends Component {
+
+	Home = props => {
+		return (
+		  <div className='App'>
+				<header className='App-header'>
+					<img src={logo} className='App-logo' alt='logo' />
+					<h1 className='App-title'>Welcome to React</h1>
+				</header>
+				<p className='App-intro'>
+				{props.title}
+				</p>
+      		</div>
+		)
+	  }
+	  
+	  NotFound = () => {
+		return (
+		  <div className='App'>
+			  <p className='intro'>
+				Page not Found!!!
+			  </p>
+			</div>
+		)
+	  }
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+	  <BrowserRouter>
+				<Switch>
+					<Route path='/signup' render={()=><this.Home title='Home Page'/>}/>
+					<Route path='/about' render={()=><this.Home title='About Page'/>}/>
+					<Route path='/help' render={()=><this.Home title='Help Page'/>}/>
+					<Route path='/services' render={()=><this.Home title='Services Page'/>}/>ok
+					<Redirect from='/' to = '/signup' exact={true} />
+					<Route component={this.NotFound}/>
+				</Switch>
+			</BrowserRouter>
     );
   }
 }
