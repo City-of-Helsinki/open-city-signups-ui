141fc5c66b57cb4867414fbdd97fa04b9193580d
Olaoluwa Alabi <ayoalabi0@gmail.com>
2018-05-21 15:26:41 +0300

    router adjusted, dropdown-hoverproperties

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -5,22 +5,16 @@ import './App.css'
 
 import LandingPage from '../Landingpage/Landing';
 import NotFound from '../Notfound/Notfound';
-import Header from '../Header/Header';
 
-const langArray = ['English', 'Suomi', 'Svenska', 'Deutsch', '日本語', 'Русский'];
 
 class App extends Component {
 
   render() {
     return (
       <div className='App'>
-        <Header languages={langArray}/>
         <BrowserRouter>
           <Switch>
             <Route path='/signup' render={()=><LandingPage title='Home Page'/>}/>
-            <Route path='/about' render={()=><LandingPage title='About Page'/>}/>
-            <Route path='/help' render={()=><LandingPage title='Help Page'/>}/>
-            <Route path='/services' render={()=><LandingPage title='Services Page'/>}/>
             <Redirect from='/' to = '/signup' exact={true} />
             <Route path='/404' render={()=><NotFound/>}/>
             <Redirect from='*' to = '/404' exact={true} />
--- src/components/Landingpage/Landing.js
+++ src/components/Landingpage/Landing.js
@@ -1,9 +1,14 @@
 import React from 'react';
 
+import Header from '../Header/Header';
+
+const langArray = ['English', 'Suomi', 'Svenska', 'Deutsch', '日本語', 'Русский'];
+
 
 const LandingPage = props =>{
   return(
     <div className='App'>
+      <Header languages={langArray}/>
       <p className='App-intro'>
         {props.title}
       </p>
