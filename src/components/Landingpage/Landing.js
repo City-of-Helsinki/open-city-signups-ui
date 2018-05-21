1013f5f77d5814febf725ca1f1689762ccc2ac27
Olaoluwa Alabi <ayoalabi0@gmail.com>
2018-05-21 16:39:37 +0300

    skeletal implementation

--- src/components/Landingpage/Landing.js
+++ src/components/Landingpage/Landing.js
@@ -1,32 +1,13 @@
 import React from 'react';
-import FontAwesomeIcon from '@fortawesome/react-fontawesome';
-import faUser from '@fortawesome/fontawesome-free-solid/faUser';
-import {FormattedMessage} from 'react-intl';
+
+import SectionOne from '../Sectionone/Sectionone';
 
 import styles from './landing.scss';
-import logo from '../../assets/logo.svg';
 
 const LandingPage = props =>{
   return(
-    <div className='App'>
-      <header className='App-header'>
-        <img src={logo} className='App-logo' alt='logo' />
-        <h1 className='App-title'><FormattedMessage id="app.welcome" defaultMessage="Welcome to React" /></h1>
-      </header>
-      <p className='App-intro'>
-        {props.title}
-      </p>
-      <FontAwesomeIcon 
-        className='super-crazy-colors'
-        name='rocket'
-        icon={faUser}
-        size='4x'
-        spin
-        style={{textShadow: '0 1px 0 rgba(0, 0, 0, 0.1)'}}/>
-
-      <section className={styles.koroBg}>
-        <p className={styles.koroContent}>Foobar</p>
-      </section>
+    <div className={styles.App}>
+      <SectionOne/>
     </div>
   );
 };
