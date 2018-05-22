a5f095f55eef8feb52c9ec3123edaf90d4b51038
Olaoluwa Alabi <ayoalabi0@gmail.com>
2018-05-22 14:24:23 +0300

    Section2 implemented with generic section and text components

--- src/components/Landingpage/Landing.js
+++ src/components/Landingpage/Landing.js
@@ -4,19 +4,19 @@ import Section from '../SectionGeneric/Section';
 
 import styles from './landing.scss';
 
-import iphone6 from '../../assets/iPhone6.png';
-const Heading = 'Yhdellä tunnuksella monta mahdollisuutta';
-const Bodytext = 'Helsingin kaupunki kokeilee ja kehittää koko ajan uusia verkkopalveluita asukkaidensa arjen helpottanmiseksi. Rekisteröitymällä oma.helsinki käyttäjäksi pääset käyttämään uusia palveluita yhdellä helpolla kirjautumisella.';
+const Heading = 'Etsimme testaajia';
+const Bodytext = 'Kokeile ja käytä helsingin uusia digitaalisia palveluita yksillä tunnuksilla. Ilmoittaudu mukaan Beta testaukseen.';
 
 const LandingPage = props =>{
   return(
     <div className={styles.App}>
       <Section 
-        bgcolor='Oma'
-        image={true}
-        img={iphone6}
         heading={Heading}
-        bodytext={Bodytext}/>
+        bodytext={Bodytext}
+        button={true}
+        buttonlabel='Signup For Beta testing'
+        buttonsize='btn btn-sm'
+        buttoncolor='info'/>
     </div>
   );
 };
