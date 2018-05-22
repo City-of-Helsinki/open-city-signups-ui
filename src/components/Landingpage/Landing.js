ecc0cb2cb26807d79f1406becb0f5ced838ed299
Olaoluwa Alabi <ayoalabi0@gmail.com>
2018-05-22 14:04:50 +0300

    Sectionone implemented using Generic section and texts components

--- src/components/Landingpage/Landing.js
+++ src/components/Landingpage/Landing.js
@@ -1,13 +1,22 @@
 import React from 'react';
 
-import SectionOne from '../Sectionone/Sectionone';
+import Section from '../SectionGeneric/Section';
 
 import styles from './landing.scss';
+
+import iphone6 from '../../assets/iPhone6.png';
+const Heading = 'Yhdellä tunnuksella monta mahdollisuutta';
+const Bodytext = 'Helsingin kaupunki kokeilee ja kehittää koko ajan uusia verkkopalveluita asukkaidensa arjen helpottanmiseksi. Rekisteröitymällä oma.helsinki käyttäjäksi pääset käyttämään uusia palveluita yhdellä helpolla kirjautumisella.';
 
 const LandingPage = props =>{
   return(
     <div className={styles.App}>
-      <SectionOne/>
+      <Section 
+        bgcolor='Oma'
+        image={true}
+        img={iphone6}
+        heading={Heading}
+        bodytext={Bodytext}/>
     </div>
   );
 };
