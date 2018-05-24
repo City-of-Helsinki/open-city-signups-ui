e0459e0e1db21b2445092d24287aab649392fd99
Jimi <jiripim@gmail.com>
2018-05-24 16:31:27 +0300

    Intl feature

--- src/redux/reducer.js
+++ src/redux/reducer.js
@@ -1,11 +1,9 @@
 import {combineReducers} from 'redux';
 import {reducer as oidcReducer} from 'redux-oidc';
 
-import {reducer as intlReducer} from '../intl/redux';
 import {reducer as apiReducer} from '../api/redux';
 
 export default combineReducers({
-  intl: intlReducer,
   oidc: oidcReducer,
   api: apiReducer,
 });
