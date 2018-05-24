f4288e98dcdc13115ce994ea0b34b698ef81da21
Jimi <jiripim@gmail.com>
2018-05-24 16:16:29 +0300

    Remove language-related stuff from header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -2,12 +2,7 @@ import React from 'react';
 import propTypes from 'prop-types';
 
 import styles from './header.scss';
-
 import HelLogo from '../../assets/helsinki-logo.svg';
-import FontAwesomeIcon from '@fortawesome/react-fontawesome';
-import faGlobe from '@fortawesome/fontawesome-free-solid/faGlobe';
-import faCaret from '@fortawesome/fontawesome-free-solid/faCaretDown';
-import {Dropdown, DropdownToggle, DropdownMenu} from 'reactstrap';
 
 class Header extends React.Component{
   constructor(props){
@@ -33,36 +28,6 @@ class Header extends React.Component{
             {this.props.logoTitle ? this.props.logoTitle : 'Oma.Helsinki'}
           </p>
         </div>
-        <div className={styles.loginSide}>
-          <div className={styles.iconContainer}>
-            <FontAwesomeIcon 
-              className={styles.userIcon}
-              icon={this.props.leftIcon ? this.props.leftIcon : faGlobe}
-              name={this.props.leftIcon ? `${this.props.leftIcon}` : 'globe'}
-            />
-            <Dropdown isOpen={this.state.dropdownOpen} toggle={this.toggle}>
-              <DropdownToggle
-                tag="span"
-                onClick={this.toggle}
-                data-toggle="dropdown"
-                aria-expanded={this.state.dropdownOpen}
-              >
-          EN
-                <FontAwesomeIcon 
-                  className={styles.caret}
-                  icon={faCaret}
-                  name='caretdown'
-                />
-              </DropdownToggle>
-              <DropdownMenu>
-                {this.props.languages.map((lang, index)=> <div 
-                  className={styles.langDiv }
-                  onClick={this.toggle}
-                  key={index}>{lang}</div>)}
-              </DropdownMenu>
-            </Dropdown>
-          </div>
-        </div>
       </div>
     );
   }
@@ -72,7 +37,6 @@ Header.propTypes = {
   logo: propTypes.node,
   logoTitle: propTypes.string,
   leftIcon: propTypes.node,
-  languages: propTypes.array,
 };
 
 export default Header;
