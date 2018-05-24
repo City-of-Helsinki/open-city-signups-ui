f5c88309a7576a55f0f6cc43d77952ff9b6faa74
Olaoluwa Alabi <ayoalabi0@gmail.com>
2018-05-24 14:25:39 +0300

    Footer social icons alignment adjusted

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -9,7 +9,6 @@ import faTwitter from '@fortawesome/fontawesome-free-brands/faTwitter';
 import styles from './footer.scss';
 
 
-
 const Footer = props =>{
   return(
     <div className={styles.koroFooter}>
@@ -17,47 +16,49 @@ const Footer = props =>{
         <img src={HelLogo} alt='Helsinki-logo' className={styles.logo}/>
       </div>
       <div className={styles.socialIconsContainer}>
-        <a 
-          href='https://www.facebook.com/myhelsinki '
-          target='_blank' 
-          rel='noopener noreferrer'>
-          <FontAwesomeIcon 
-            name = 'facebook'
-            size ='2x'
-            icon = {faFacebook}
-            className={styles.icons}
-          />
-        </a>
-        <a 
-          href='https://www.instagram.com/myhelsinki'
-          target='_blank'
-          rel='noopener noreferrer'>
-          <FontAwesomeIcon 
-            name = 'instagram'
-            size ='2x'
-            icon = {faInstagram}
-            className={styles.icons}/>
-        </a>
-        <a 
-          href='https://www.youtube.com/user/Visithelsinki' 
-          target='_blank'
-          rel='noopener noreferrer'>
-          <FontAwesomeIcon 
-            name = 'youtube'
-            size ='2x'
-            icon = {faYoutube}
-            className={styles.icons}/>
-        </a>
-        <a 
-          href='https://twitter.com/myhelsinki' 
-          target='_blank' 
-          rel='noopener noreferrer'>
-          <FontAwesomeIcon 
-            name = 'twitter'
-            size ='2x'
-            icon = {faTwitter}
-            className={styles.icons}/>
-        </a>
+        <div className={styles.footericons}>
+          <a 
+            href='https://www.facebook.com/myhelsinki '
+            target='_blank' 
+            rel='noopener noreferrer'>
+            <FontAwesomeIcon 
+              name = 'facebook'
+              size ='2x'
+              icon = {faFacebook}
+              className={styles.icons}
+            />
+          </a>
+          <a 
+            href='https://www.instagram.com/myhelsinki'
+            target='_blank'
+            rel='noopener noreferrer'>
+            <FontAwesomeIcon 
+              name = 'instagram'
+              size ='2x'
+              icon = {faInstagram}
+              className={styles.icons}/>
+          </a>
+          <a 
+            href='https://www.youtube.com/user/Visithelsinki' 
+            target='_blank'
+            rel='noopener noreferrer'>
+            <FontAwesomeIcon 
+              name = 'youtube'
+              size ='2x'
+              icon = {faYoutube}
+              className={styles.icons}/>
+          </a>
+          <a 
+            href='https://twitter.com/myhelsinki' 
+            target='_blank' 
+            rel='noopener noreferrer'>
+            <FontAwesomeIcon 
+              name = 'twitter'
+              size ='2x'
+              icon = {faTwitter}
+              className={styles.icons}/>
+          </a>
+        </div>
       </div>
     </div>
   );
