09fb7292eeb49976d69c551dc30400ca675a1319
Jimi <jiripim@gmail.com>
2018-05-25 14:38:51 +0300

    Footer to use bootstrap responsive grid

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -1,66 +1,56 @@
 import React from 'react';
-
-import HelLogo from '../../assets/helsinki-logo.1.svg';
+import {Col, Container, Row} from 'reactstrap';
 import FontAwesomeIcon from '@fortawesome/react-fontawesome';
-import faFacebook from '@fortawesome/fontawesome-free-brands/faFacebookF';
+import faFacebook from '@fortawesome/fontawesome-free-brands/faFacebook';
+import faTwitter from '@fortawesome/fontawesome-free-brands/faTwitter';
 import faInstagram from '@fortawesome/fontawesome-free-brands/faInstagram';
 import faYoutube from '@fortawesome/fontawesome-free-brands/faYoutube';
-import faTwitter from '@fortawesome/fontawesome-free-brands/faTwitter';
+
+import HelLogo from '../../assets/helsinki-logo.1.svg';
 import styles from './footer.scss';
 
+const socialIcons = [
+  {
+    href: 'https://www.facebook.com/myhelsinki',
+    icon: faFacebook,
+  },
+  {
+    href: 'https://www.instagram.com/myhelsinki',
+    icon: faInstagram,
+  },
+  {
+    href: 'https://www.youtube.com/user/Visithelsinki',
+    icon: faYoutube,
+  },
+  {
+    href: 'https://twitter.com/myhelsinki',
+    icon: faTwitter,
+  },
+];
 
-const Footer = props =>{
+const Footer = () => {
   return(
-    <div className={styles.koroFooter}>
-      <div className={styles.logoContainer}>
-        <img src={HelLogo} alt='Helsinki-logo' className={styles.logo}/>
-      </div>
-      <div className={styles.socialIconsContainer}>
-        <div className={styles.footericons}>
-          <a 
-            href='https://www.facebook.com/myhelsinki '
-            target='_blank' 
-            rel='noopener noreferrer'>
-            <FontAwesomeIcon 
-              name = 'facebook'
-              size ='2x'
-              icon = {faFacebook}
-              className={styles.icons}
-            />
-          </a>
-          <a 
-            href='https://www.instagram.com/myhelsinki'
-            target='_blank'
-            rel='noopener noreferrer'>
-            <FontAwesomeIcon 
-              name = 'instagram'
-              size ='2x'
-              icon = {faInstagram}
-              className={styles.icons}/>
-          </a>
-          <a 
-            href='https://www.youtube.com/user/Visithelsinki' 
-            target='_blank'
-            rel='noopener noreferrer'>
-            <FontAwesomeIcon 
-              name = 'youtube'
-              size ='2x'
-              icon = {faYoutube}
-              className={styles.icons}/>
-          </a>
-          <a 
-            href='https://twitter.com/myhelsinki' 
-            target='_blank' 
-            rel='noopener noreferrer'>
-            <FontAwesomeIcon 
-              name = 'twitter'
-              size ='2x'
-              icon = {faTwitter}
-              className={styles.icons}/>
-          </a>
-        </div>
-      </div>
-    </div>
+    <footer className={styles.footer}>
+      <Container>
+        <Row className='justify-content-center'>
+          <Col xs='auto'>
+            <img src={HelLogo} alt='Helsinki-logo' className={styles.logo}/>
+          </Col>
+        </Row>
+        <Row className='justify-content-center'>
+          {socialIcons.map(item => {
+            return (
+              <Col xs={true} sm='auto' key={item.href}>
+                <a href={item.href} target='_blank' rel='noopener noreferrer' className={styles.icon}>
+                  <FontAwesomeIcon size='2x' icon={item.icon}/>
+                </a>
+              </Col>
+            );
+          })}
+        </Row>
+      </Container>
+    </footer>
   );
 };
+
 export default Footer;
